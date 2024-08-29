deff44920148007c18338d889caaa3d5e0f46f46
praewptr <panthira.ruj@gmail.com>
2024-08-29 17:11:45 +0700

    feat: Add script to manage Docker files

--- .copier/manage_docker.py
+++ .copier/manage_docker.py
@@ -1,7 +1,15 @@
 import shutil
 import os
 
-file_to_copy = './docker-compose.override.yml'
+
+def file_to_copy(file, target):
+    shutil.copy(file, target)
+    return {"copied_file": file}
+
 
 os.makedirs('./docker_test', exist_ok=True)
-shutil.copy(file_to_copy, './docker_test')
+
+file_to_copy('./docker-compose.override.yml', './docker_test')
+file_to_copy('./docker-compose.yml', './docker_test')
+file_to_copy('./docker-compose.postgres.yml', './docker_test')
+file_to_copy('./docker-traefik.yml', './docker_test')
