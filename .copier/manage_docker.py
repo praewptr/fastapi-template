f8dd50c757270714ec19a5d151431ae246a2a934
praewptr <panthira.ruj@gmail.com>
2024-08-29 17:37:53 +0700

    Refactor manage_docker.py to improve Docker file management

--- .copier/manage_docker.py
+++ .copier/manage_docker.py
@@ -8,14 +8,23 @@ def copy_file(source, destination):
     shutil.copy(source, destination)
 
 
+def create_empty_file(destination):
+    with open(destination, 'w') as f:
+        pass  # Create an empty file
+
+
 answers_path = Path(__file__).parent / ".copier-answers.yml"
 answers = json.loads(answers_path.read_text())
-for key, value in answers.items():
-    if key == 'traefik':
-        if value:
-            os.makedirs('./docker_test', exist_ok=True)
 
-            copy_file('./docker-compose.override.yml', './docker_test')
-            copy_file('./docker-compose.yml', './docker_test')
-            copy_file('./docker-compose.postgres.yml', './docker_test')
-            copy_file('./docker-compose.traefik.yml', './docker_test')
+os.makedirs('./docker_test', exist_ok=True)
+
+if answers.get('traefik', False):
+    copy_file('./docker-compose.override.yml', './docker_test')
+    copy_file('./docker-compose.yml', './docker_test')
+    copy_file('./docker-compose.postgres.yml', './docker_test')
+    copy_file('./docker-compose.traefik.yml', './docker_test')
+else:
+    create_empty_file('./docker_test/docker-compose.override.yml')
+    create_empty_file('./docker_test/docker-compose.yml')
+    create_empty_file('./docker_test/docker-compose.postgres.yml')
+    create_empty_file('./docker_test/docker-compose.traefik.yml')
