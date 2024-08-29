782aaa24d23726841cdb7bb803d8f30225b56a4d
praewptr <panthira.ruj@gmail.com>
2024-08-29 17:30:01 +0700

    Refactor manage_docker.py to use more descriptive function name

--- .copier/manage_docker.py
+++ .copier/manage_docker.py
@@ -1,14 +1,21 @@
 import shutil
 import os
+from pathlib import Path
+import json
 
 
 def copy_file(source, destination):
     shutil.copy(source, destination)
 
 
-os.makedirs('./docker_test', exist_ok=True)
+answers_path = Path(__file__).parent / ".copier-answers.yml"
+answers = json.loads(answers_path.read_text())
+for key, value in answers.items():
+    if key == 'traefik':
+        if value:
+            os.makedirs('./docker_test', exist_ok=True)
 
-copy_file('./docker-compose.override.yml', './docker_test')
-copy_file('./docker-compose.yml', './docker_test')
-copy_file('./docker-compose.postgres.yml', './docker_test')
-copy_file('./docker-compose.traefik.yml', './docker_test')
+            copy_file('./docker-compose.override.yml', './docker_test')
+            copy_file('./docker-compose.yml', './docker_test')
+            copy_file('./docker-compose.postgres.yml', './docker_test')
+            copy_file('./docker-compose.traefik.yml', './docker_test')
